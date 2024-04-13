d4705eb7ce8a922586a839b88ee853020c3e401e
Ruby Casey <ruby@cxsey.com>
2024-04-13 17:54:46 -0400

    Get player choice

--- javascript.js
+++ javascript.js
@@ -1,9 +1,26 @@
 let computerSelection;
 
 getComputerChoice();
+getPlayerChoice();
 
 function getComputerChoice() {
-    computerSelection = Math.round(Math.random() * (2 - 0) + 0);
-    //console.log(computerSelection)
+    randomSelection = Math.round(Math.random() * (2 - 0) + 0);
+    switch (randomSelection) {
+        case 0:
+            computerSelection = "rock";
+            break;
+        case 1:
+            computerSelection = "paper";
+            break;
+        case 2:
+            computerSelection = "scissors";
+            break;
+    };
+    console.log(computerSelection)
     return computerSelection;
+};
+
+function getPlayerChoice() {
+    playerInput = prompt("Enter Rock, Paper, or Scissors.").toLowerCase();
+    console.log(playerInput);
 };
